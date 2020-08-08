ba8a27c81270284b31b995d6ebb653392addff4e
thomasfregene <fregzythomas@gmail.com>
2020-08-08 14:01:03 +0100

    React Transition Type Transition Group -Tgroup.js

--- src/components/Tgroup.js
+++ src/components/Tgroup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 import '../css/App.css';
 
 class Slide extends Component{
@@ -6,22 +7,52 @@ class Slide extends Component{
         items:[]
     }
     
-    addElements () {
-      
+    showElements () {
+      return this.state.items.map((item,index)=>(
+          <CSSTransition
+          classNames="item"
+          timeout={200}
+          key={index}
+          onEntered={(node)=>{
+              node.classList.add('active')
+          }}
+          >
+               <div className="item" key={index}>{item}</div>   
+          </CSSTransition>
+         
+      ))
     }
 
     generateNumber(){
-       
+       let newArray = [
+           ...this.state.items, 
+           Math.floor(Math.random()*100) +1
+       ];
+
+       this.setState({
+           items: newArray
+       })
     }
 
     removeNumber(){
-      
+      let newArray = this.state.items.slice(0,-1);
+
+      this.setState({
+        items: newArray
+    })
     }
 
     render(){
+        console.log(this.state.items)
         return(
             <div>
-                {this.addElements()}
+                <TransitionGroup
+                className="list"
+                component="div"
+                >
+                {this.showElements()}
+                </TransitionGroup>
+                
              
                 <div className="btns">
                     <div className="btn-add" onClick={()=> this.generateNumber()}>Add Elements</div>
